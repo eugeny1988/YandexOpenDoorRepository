27452e4467e6bf6f982aeef4c58e4888de046e19
eugeny1988 <eugeny1988@gmail.com>
2022-08-21 11:17:05 +0300

    Предварительные изменения

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,40 @@
 fun main() {
     val s = checkNotNull(readLine())
-    var stringNumber: String = ""
     var newNumbersList = mutableListOf<String>()
     var final = 0
     for (i in s.indices) {
 
-        if (s[i].isDigit()) {
+        if (s[i].isDigit() && s[i - 1].isLetter()) {
             newNumbersList.add(s[i].toString())
             try {
-                if (s[i + 1].isDigit())
+                if (s[i + 1].isDigit()) {
                     newNumbersList[newNumbersList.lastIndex] += (s[i + 1].toString())
+                    println(newNumbersList[newNumbersList.lastIndex])
+                } else
+                    if (!s[i].isDigit() && s[i + 1].isLetter())
+                        newNumbersList.add("1")
 
-            } catch (error: StringIndexOutOfBoundsException) {
-
+            } catch (e: StringIndexOutOfBoundsException) {
+                continue
             }
+        } else {
+            try {
+                if (s[i + 1].isLetter()) {
+                    newNumbersList.add("1")
+                }
+            } catch (e: StringIndexOutOfBoundsException) {
+                newNumbersList.add("1")
+                continue
+            }
+
+
         }
-        else
-            newNumbersList.add("1")
+
     }
     for (k in newNumbersList) {
         final += k.toInt()
     }
+    println(newNumbersList)
     println(final)
 }
 
